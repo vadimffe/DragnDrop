e298ec66a2dc2878044d4ceec0421e2a7b400ae9
vadimffe <vadim.ffegmail.com>
2021-04-29 23:19:57 +0300

    Added ContextMenu

--- DragnDrop/MainWindow.xaml.cs
+++ DragnDrop/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using DragnDrop.Models;
 using System;
 using System.Collections;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -19,32 +21,90 @@ namespace DragnDrop
       InitializeComponent();
     }
 
-    ListBox dragSource = null;
+    private ObservableCollection<string> runningApplications;
+    private TreeViewItem draggedItem;
+    private ListBox? dragSource = null;
+    private object mydata;
+
+    private void OnPreviewMouseRightButtonDown(object sender, MouseButtonEventArgs e)
+    {
+      TreeViewItem treeViewItem = VisualUpwardSearch(e.OriginalSource as DependencyObject);
+
+      TreeViewItem f = e.Source as TreeViewItem;
+
+      if (treeViewItem != null)
+      {
+        treeViewItem.Focus();
+        e.Handled = true;
+
+        ItemsControl parent = ItemsControl.ItemsControlFromItemContainer(f);
+        if (parent != null)
+        {
+          parent.Items.Remove(f);
+        }
+      }
+    }
+
+    static TreeViewItem VisualUpwardSearch(DependencyObject source)
+    {
+      while (source != null && !(source is TreeViewItem))
+        source = VisualTreeHelper.GetParent(source);
+
+      return source as TreeViewItem;
+    }
 
     private void TreeView_Drop(object sender, DragEventArgs e)
     {
-      TreeView parent = (TreeView)sender;
-      object data = e.Data.GetData(typeof(string));
-      ((IList)dragSource.ItemsSource).Remove(data);
-      //parent.Items.Add(data);
-      Debug.WriteLine(data);
+      e.Effects = DragDropEffects.None;
+      e.Handled = true;
+      this.draggedItem = new TreeViewItem() { Header = this.mydata };
+      TreeViewItem targetItem = this.GetNearestContainer(e.OriginalSource as UIElement);
+      if (targetItem != null && this.draggedItem != null)
+      {
+        e.Effects = DragDropEffects.Move;
+
+        CategoryItem target = (CategoryItem)targetItem.Header;
+
+        CategoryItem draggedTreeItem = new CategoryItem(string.Empty) { ItemName = this.mydata.ToString() };
+        this.AddChild(draggedTreeItem, target);
+        this.runningApplications.Remove(this.mydata.ToString());
+      }
+    }
+
+    public void AddChild(CategoryItem sourceItem, CategoryItem targetItem)
+    {
+      if (targetItem == null)
+      {
+        return;
+      }
+
+      targetItem.CategoryItems.Add(sourceItem);
+
+      if (sourceItem.CategoryItems != null)
+      {
+        foreach (CategoryItem item in sourceItem.CategoryItems)
+        {
+          this.AddChild(item, sourceItem);
+        }
+      }
     }
 
     private void ListBox_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
     {
       ListBox parent = (ListBox)sender;
-      dragSource = parent;
-      object data = GetDataFromListBox(dragSource, e.GetPosition(parent));
+      this.dragSource = parent;
+      this.runningApplications = parent.ItemsSource as ObservableCollection<string>;
+      this.mydata = GetDataFromListBox(this.dragSource, e.GetPosition(parent));
 
-      if (data != null)
+      if (this.mydata != null)
       {
-        DragDrop.DoDragDrop(parent, data, DragDropEffects.Move);
+        DragDrop.DoDragDrop(parent, this.mydata, DragDropEffects.Move);
       }
     }
 
-    private static object GetDataFromListBox(ListBox source, Point point)
+    private static object? GetDataFromListBox(ListBox source, Point point)
     {
-      UIElement element = source.InputHitTest(point) as UIElement;
+      UIElement? element = source.InputHitTest(point) as UIElement;
       if (element != null)
       {
         object data = DependencyProperty.UnsetValue;
@@ -72,37 +132,16 @@ namespace DragnDrop
       return null;
     }
 
-    // Recursive call
-    private bool TryGetSelectedNode(
-      CategoryItem parentNode,
-      out (CategoryItem Parent, CategoryItem SelectedChild) selectedNode)
+    private TreeViewItem GetNearestContainer(UIElement? element)
     {
-      selectedNode = (null, null);
-      if (parentNode == null)
+      TreeViewItem? container = element as TreeViewItem;
+      while ((container == null) && (element != null))
       {
-        return false;
+        element = VisualTreeHelper.GetParent(element) as UIElement;
+        container = element as TreeViewItem;
       }
 
-      if (parentNode.IsSelected)
-      {
-        selectedNode.SelectedChild = parentNode;
-        return true;
-      }
-
-      foreach (CategoryItem childNode in parentNode.CategoryItems)
-      {
-        if (this.TryGetSelectedNode(childNode, out selectedNode))
-        {
-          if (selectedNode.Parent == null)
-          {
-            selectedNode.Parent = parentNode;
-          }
-
-          return true;
-        }
-      }
-
-      return false;
+      return container;
     }
   }
 }
